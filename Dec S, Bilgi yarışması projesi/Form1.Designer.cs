f321ab5115346c356d58916836f6075b2764039b
UFatih <fatihkusaslan@yahoo.com>
2023-08-10 13:54:10 +0300

    Add project files.

---
+++ Dec S, Bilgi yarışması projesi/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace Dec_S__Bilgi_yarışması_projesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblsoruno = new System.Windows.Forms.Label();
+            this.lbldoğru = new System.Windows.Forms.Label();
+            this.lblyanlış = new System.Windows.Forms.Label();
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.buttonsonraki = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(this.richTextBox1, "richTextBox1");
+            this.richTextBox1.Name = "richTextBox1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Name = "label3";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblsoruno
+            // 
+            resources.ApplyResources(this.lblsoruno, "lblsoruno");
+            this.lblsoruno.Name = "lblsoruno";
+            // 
+            // lbldoğru
+            // 
+            resources.ApplyResources(this.lbldoğru, "lbldoğru");
+            this.lbldoğru.Name = "lbldoğru";
+            // 
+            // lblyanlış
+            // 
+            resources.ApplyResources(this.lblyanlış, "lblyanlış");
+            this.lblyanlış.Name = "lblyanlış";
+            // 
+            // buttonA
+            // 
+            this.buttonA.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            resources.ApplyResources(this.buttonA, "buttonA");
+            this.buttonA.Name = "buttonA";
+            this.buttonA.UseVisualStyleBackColor = false;
+            this.buttonA.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonB
+            // 
+            this.buttonB.BackColor = System.Drawing.Color.Honeydew;
+            resources.ApplyResources(this.buttonB, "buttonB");
+            this.buttonB.Name = "buttonB";
+            this.buttonB.UseVisualStyleBackColor = false;
+            this.buttonB.Click += new System.EventHandler(this.buttonB_Click);
+            // 
+            // buttonC
+            // 
+            this.buttonC.BackColor = System.Drawing.Color.LightCyan;
+            resources.ApplyResources(this.buttonC, "buttonC");
+            this.buttonC.Name = "buttonC";
+            this.buttonC.UseVisualStyleBackColor = false;
+            this.buttonC.Click += new System.EventHandler(this.buttonC_Click);
+            // 
+            // buttonD
+            // 
+            this.buttonD.BackColor = System.Drawing.Color.LavenderBlush;
+            this.buttonD.ForeColor = System.Drawing.SystemColors.ControlText;
+            resources.ApplyResources(this.buttonD, "buttonD");
+            this.buttonD.Name = "buttonD";
+            this.buttonD.UseVisualStyleBackColor = false;
+            this.buttonD.Click += new System.EventHandler(this.buttonD_Click);
+            // 
+            // buttonsonraki
+            // 
+            this.buttonsonraki.BackColor = System.Drawing.Color.MediumTurquoise;
+            resources.ApplyResources(this.buttonsonraki, "buttonsonraki");
+            this.buttonsonraki.Name = "buttonsonraki";
+            this.buttonsonraki.UseVisualStyleBackColor = false;
+            this.buttonsonraki.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox3
+            // 
+            resources.ApplyResources(this.pictureBox3, "pictureBox3");
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.buttonsonraki);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonA);
+            this.Controls.Add(this.lblyanlış);
+            this.Controls.Add(this.lbldoğru);
+            this.Controls.Add(this.lblsoruno);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblsoruno;
+        private System.Windows.Forms.Label lbldoğru;
+        private System.Windows.Forms.Label lblyanlış;
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonD;
+        private System.Windows.Forms.Button buttonsonraki;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Dec S, Bilgi yarışması projesi/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dec_S__Bilgi_yarışması_projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int soruno, doğru, yanlış;
+
+        private void buttonB_Click(object sender, EventArgs e)
+        {
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+            buttonsonraki.Enabled = true;
+
+            label5.Text = buttonB.Text;
+            if (label4.Text == label5.Text)
+            {
+                doğru++;
+                lbldoğru.Text = doğru.ToString();
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                yanlış++;
+                lblyanlış.Text = yanlış.ToString();
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+            buttonsonraki.Enabled = true;
+
+            label5.Text = buttonC.Text;
+            if (label4.Text == label5.Text)
+            {
+                doğru++;
+                lbldoğru.Text = doğru.ToString();
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                yanlış++;
+                lblyanlış.Text = yanlış.ToString();
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void buttonD_Click(object sender, EventArgs e)
+        {
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+            buttonsonraki.Enabled = true;
+
+            label5.Text = buttonD.Text;
+            if (label4.Text == label5.Text)
+            {
+                doğru++;
+                lbldoğru.Text = doğru.ToString();
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                yanlış++;
+                lblyanlış.Text = yanlış.ToString();
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+            buttonsonraki.Enabled = true;
+
+            label5.Text = buttonA.Text;
+            if (label4.Text == label5.Text)
+            {
+                doğru++;
+                lbldoğru.Text = doğru.ToString();
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                yanlış++;
+                lblyanlış.Text = yanlış.ToString();
+                pictureBox1.Visible = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            buttonA.Enabled = true;
+            buttonB.Enabled = true;
+            buttonC.Enabled = true;
+            buttonD.Enabled = true;
+            buttonsonraki.Enabled = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+
+            soruno++;
+            lblsoruno.Text = soruno.ToString();
+
+            if (soruno == 0)
+            {
+
+                richTextBox1.Text = "Sonraki butonuna basıp yarışmaya başlayabilirsiniz";
+            }
+
+            if (soruno == 1)
+            {
+                richTextBox1.Text = "Türkiye Süper Lig takımlarından hangisi 2000 yılında Arsenal takımını yenerek Uefa kupasını müzesine götürmüştür?";
+                buttonA.Text = "Galatasaray";
+                buttonB.Text = "Fenerbahçe";
+                buttonC.Text = "Beşiktaş J.K.";
+                buttonD.Text = "Trabzonspor";
+                label4.Text = "Galatasaray";
+            }
+
+            if (soruno== 2)
+            {
+                richTextBox1.Text = "Cristiano Ronaldo aşağıdaki takımların hangisinde forma giymemiştir?";
+                buttonA.Text = "Real Madrid";
+                buttonB.Text = "Manchester United";
+                buttonC.Text = "Sporting CP";
+                buttonD.Text = "Bayern Munich";
+                label4.Text = "Bayern Munich";
+            }
+            if (soruno == 3)
+            {
+                richTextBox1.Text = "Radamel Falcao, Porto takımıyla hangi takıma karşı gol atıp takımının maçı 1-0 ile galip gelmesini sağlayarak Uefa kupasını kaldırmıştır?";
+                buttonA.Text = "Chelsea";
+                buttonB.Text = "Braga";
+                buttonC.Text = "Sevilla";
+                buttonD.Text = "Liverpool";
+                label4.Text = "Braga";
+            }
+            if (soruno == 4)
+            {
+                richTextBox1.Text = "Hangi futbolcu Barcelona takımında forma giymemiştir?";
+                buttonA.Text = "Rüştü Reçber";
+                buttonB.Text = "Samuel Eto'o";
+                buttonC.Text = "Ronaldinho";
+                buttonD.Text = "Roberto Carlos";
+                label4.Text = "Roberto Carlos";
+                
+
+            }
+            if (soruno == 5)
+            {
+                buttonsonraki.Text = "Sonuçlar";
+                buttonA.Enabled = false;
+                buttonB.Enabled = false;
+                buttonC.Enabled = false;
+                buttonD.Enabled = false;
+                buttonsonraki.Enabled = false;
+                MessageBox.Show("Doğru:" + doğru + "\n" + "Yanlış:" + yanlış);
+                    
+            }
+
+        }
+    }
+}
